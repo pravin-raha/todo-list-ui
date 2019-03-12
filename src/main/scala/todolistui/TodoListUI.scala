3b1cf62e27f517ced88ecc4edea566b190c1a455
Pravin <pravinrahangdale41@gmail.com>
2019-03-13 02:33:39 +0530

    Added todo ui

--- src/main/scala/todolistui/TodoListUI.scala
+++ src/main/scala/todolistui/TodoListUI.scala
@@ -1,12 +1,68 @@
 package todolistui
 
-import outwatch.dom._
-import outwatch.dom.dsl._
+import cats.effect.IO
 import monix.execution.Scheduler.Implicits.global
+import outwatch.dom._
+import cats.implicits._
+import outwatch.dom.dsl._
 
 object TodoListUI {
-  def main(args: Array[String]): Unit = {
 
-    OutWatch.renderInto("#app", h1("Hello World")).unsafeRunSync()
+  val todoPrograme: IO[VNode] = for {
+    addEvents <- Handler.create[String]()
+    deleteEvents <- Handler.create[String]()
+    additions <- IO(addEvents.map(addToList))
+    deletions <- IO(deleteEvents.map(removeFromList))
+    state = (additions ++ deletions)
+      .scan(List.empty[String])((list, fn) => fn.apply(list))
+    listViewsF = state
+      .map(_.map(todo => todoComponent(todo, deleteEvents)).sequence)
+    listViews <- IO(listViewsF.map(f => f.unsafeRunSync))
+    textFieldCom <- textFieldComponent(addEvents)
+    todoDiv <- IO(
+      div(
+        textFieldCom,
+        ul(children <-- listViews)
+      )
+    )
+  } yield todoDiv
+
+
+  def textFieldComponent(outputEvents: Sink[String]): IO[VNode] = {
+    for {
+      todo <- Handler.create[String]()
+      inputDiv <- IO(div(
+        label("Todo: "),
+        input(onInput.value --> todo),
+        button(
+          onClick(todo) --> outputEvents,
+          "Submit"
+        )
+      ))
+    } yield inputDiv
+  }
+
+  def todoComponent(todo: String, deleteEvents: Sink[String]): IO[VNode] = {
+    IO(li(
+      span(todo),
+      button(onClick(todo) --> deleteEvents, "Delete")
+    ))
+  }
+
+  def addToList(todo: String): List[String] => List[String] = {
+    list: List[String] => list :+ todo
+  }
+
+  def removeFromList(todo: String): List[String] => List[String] = {
+    list: List[String] => list.filterNot(_ == todo)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val app = for {
+      todo <- todoPrograme
+      _ <- OutWatch.renderInto("#app", todo)
+    } yield ()
+
+    app.unsafeRunSync()
   }
 }
